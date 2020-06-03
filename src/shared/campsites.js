2e59194780aee22fd112e010416916a1a145071b
Joseph Bernadas <jb3rns@gmail.com>
2020-06-02 20:44:53 -0700

    Fixed extra info added un-officially

--- src/shared/campsites.js
+++ src/shared/campsites.js
@@ -9,7 +9,7 @@ export const CAMPSITES = [
             {
                 id: 0,
                 rating: 5,
-                text: "What a magnificent view Joseph!",
+                text: "What a magnificent view!",
                 author: "Tinus Lorvaldes",
                 date: "2018-10-25T16:30Z"
             },
@@ -53,7 +53,7 @@ export const CAMPSITES = [
             {
                 id: 0,
                 rating: 5,
-                text: "What a magnificent view cabron!",
+                text: "What a magnificent view!",
                 author: "Tinus Lorvaldes",
                 date: "2018-10-25T16:30Z"
             },
@@ -97,7 +97,7 @@ export const CAMPSITES = [
             {
                 id: 0,
                 rating: 5,
-                text: "What a magnificent view carnal!",
+                text: "What a magnificent view!",
                 author: "Tinus Lorvaldes",
                 date: "2018-10-25T16:30Z"
             },
@@ -141,7 +141,7 @@ export const CAMPSITES = [
             {
                 id: 0,
                 rating: 5,
-                text: "What a magnificent view guey!",
+                text: "What a magnificent view!",
                 author: "Tinus Lorvaldes",
                 date: "2018-10-25T16:30Z"
             },
